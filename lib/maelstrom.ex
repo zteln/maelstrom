8f29ec18e9831b52101eb766b9d7b34b377fbc7a
Simon Lentz <simonemlentz@gmail.com>
2026-04-06 21:14:25 +0200

    update Maelstrom to wrap Agent

--- lib/maelstrom.ex
+++ lib/maelstrom.ex
@@ -1,97 +1,155 @@
 defmodule Maelstrom do
-  @moduledoc """
-  Injects `run/0` into the calling module and requires a callback `apply_msg/2`.
-  `apply_msg/2` receives a message from the Maelstrom binary as the first argument, and a keyword list as the second. The keyword list `opts` contains `:owner` which is the PID of the calling module.
-  """
+  @moduledoc false
+
   defstruct [
-    :owner,
-    :m_f,
+    :src,
+    :dest,
     :node_id,
-    :node_ids
+    :node_ids,
+    :in_msg_id,
+    waiting_rpcs: %{},
+    msg_id: 0
   ]
 
-  @callback apply_msg(msg :: map(), opts :: keyword()) :: map()
+  @type t :: %__MODULE__{}
 
-  defmacro __using__(opts) do
+  @callback init_state() :: any()
+  @callback handle_msg(
+              body :: map(),
+              state :: any(),
+              meta :: map()
+            ) ::
+              {:noreply, any()}
+              | {:noreply, any(), t()}
+              | {:reply, map(), any()}
+              | {:reply, map(), any(), t()}
+
+  defmacro __using__(_opts) do
     quote do
       @behaviour unquote(__MODULE__)
+      @before_compile unquote(__MODULE__)
+      use Agent
 
-      def run() do
-        owner = self()
-        m_f = {__MODULE__, :apply_msg}
-
-        Task.start_link(fn ->
-          IO.stream()
-          |> Stream.transform(
-            %Maelstrom{m_f: m_f, owner: owner},
-            fn input, state ->
-              debug_msg(input)
-              state = Maelstrom.process_input(state, input)
-              {[], state}
-            end
-          )
-          |> Stream.run()
-        end)
+      @impl Maelstrom
+      def handle_msg(%{type: "init"} = body, state, meta) do
+        meta = %{meta | node_id: body.node_id, node_ids: body.node_ids}
+        body = %{type: "init_ok"}
+        {:reply, body, state, meta}
       end
 
-      if unquote(opts[:debug]) do
-        defp debug_msg(input) do
-          unquote(opts[:debug_file] || Path.join(File.cwd!(), "maelstrom.log"))
-          |> File.write!(input, [:append])
+      def handle_msg(%{in_reply_to: msg_id} = resp_body, state, meta) do
+        %{waiting_rpcs: waiting_rpcs} = meta
+
+        case Map.pop(waiting_rpcs, msg_id) do
+          {nil, _waiting_rpcs} ->
+            {:noreply, state}
+
+          {{resp_callback, body}, waiting_rpcs} ->
+            meta = %{meta | waiting_rpcs: waiting_rpcs}
+            resp_callback.(body, resp_body, state, meta)
         end
-      else
-        defp debug_msg(input), do: :ok
       end
     end
   end
 
-  def process_input(state, input) do
-    msg = decode(input)
-    {reply, state} = process_msg(state, msg)
+  defmacro __before_compile__(_opts) do
+    quote do
+      def handle_msg(_body, state, _meta) do
+        {:noreply, state}
+      end
+    end
+  end
 
-    reply
+  def start_link(mod) do
+    Agent.start_link(fn ->
+      me = self()
+      state = mod.init_state()
+      meta = %__MODULE__{}
+
+      start_io_stream(fn msg ->
+        msg = decode(msg)
+
+        Agent.update(me, fn {state, meta} ->
+          meta = %{
+            meta
+            | src: msg.src,
+              dest: msg.dest,
+              in_msg_id: get_in(msg, [:body, :msg_id])
+          }
+
+          {body, state, meta} =
+            case mod.handle_msg(msg.body, state, meta) do
+              {:noreply, state} -> {nil, state, meta}
+              {:noreply, state, meta} -> {nil, state, meta}
+              {:reply, reply, state} -> {reply, state, meta}
+              {:reply, reply, state, meta} -> {reply, state, meta}
+            end
+
+          if body do
+            body
+            |> into_reply(meta)
+            |> encode()
+            |> write_to_stdout()
+          end
+
+          {state, %{meta | msg_id: meta.msg_id + 1}}
+        end)
+      end)
+
+      {state, meta}
+    end)
+  end
+
+  @doc "Sends a fire-and-forget message."
+  def cast(src, dest, body, meta) do
+    %{src: src, dest: dest, body: body}
     |> encode()
-    |> send_reply()
+    |> write_to_stdout()
 
-    state
+    meta
   end
 
-  # initialize Maelstrom
-  defp process_msg(state, %{body: %{type: "init"}} = msg) do
+  @doc "Sends an RPC message, expecting a reply."
+  def call(src, dest, body, response_callback, meta) do
+    body = Map.put(body, :msg_id, meta.msg_id)
+
+    %{src: src, dest: dest, body: body}
+    |> encode()
+    |> write_to_stdout()
+
+    waiting_rpcs = Map.put(meta.waiting_rpcs, meta.msg_id, response_callback)
+
+    %{meta | msg_id: meta.msg_id + 1, waiting_rpcs: waiting_rpcs}
+  end
+
+  @doc "Writes the term to STDERR, returning the unchanged argument."
+  def debug(term) do
+    term
+    |> encode()
+    |> IO.warn()
+
+    term
+  end
+
+  defp start_io_stream(callback) do
+    Task.start_link(fn ->
+      IO.stream()
+      |> Stream.each(&callback.(&1))
+      |> Stream.run()
+    end)
+  end
+
+  defp into_reply(body, meta) do
+    body =
+      body
+      |> Map.put(:in_reply_to, meta.in_msg_id)
+      |> Map.put(:msg_id, meta.msg_id)
+
     %{
-      body:
-        %{
-          node_id: node_id,
-          node_ids: node_ids
-        } = body
-    } = msg
-
-    body = Map.put(body, :type, "init_ok")
-    reply = %{msg | body: body} |> prepare()
-    {reply, %{state | node_id: node_id, node_ids: node_ids}}
-  end
-
-  # let client handle all other messages
-  defp process_msg(state, msg) do
-    %{m_f: {mod, fun}} = state
-    opts = [owner: state.owner, node_id: state.node_id, node_ids: state.node_ids]
-    args = [msg, opts]
-
-    reply =
-      apply(mod, fun, args)
-      |> prepare()
-
-    {reply, state}
-  end
-
-  defp send_reply(reply) do
-    IO.puts(reply)
-  end
-
-  defp prepare(msg) do
-    %{src: src, dest: dest, body: %{msg_id: msg_id} = body} = msg
-    body = Map.put(body, :in_reply_to, msg_id)
-    %{src: dest, dest: src, body: body}
+      src: meta.dest,
+      dest: meta.src,
+      body: body
+    }
   end
 
   defp decode(input) do
@@ -104,4 +162,6 @@ defmodule Maelstrom do
   end
 
   defp encode(term), do: JSON.encode!(term)
+
+  defp write_to_stdout(term), do: IO.puts(term)
 end
